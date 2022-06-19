4992e1f362ab19dae375fb0a2784b99e03dd0b90
Marcel Seweryn <masewera@artemis.wszib.edu.pl>
2022-06-19 02:52:47 +0200

    Weryfikacja wejscia: warunek istnienia trójkąta.

--- trojkaty.java
+++ trojkaty.java
@@ -9,6 +9,7 @@ class Trojkaty {
      * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
+
     public static void jakiTrojkat(float a, float b, float c){
         if (a == b && b == c && a == c) {
             System.out.println("Trójkąt równoboczny"); 
@@ -20,6 +21,13 @@ class Trojkaty {
             System.out.println("Trójkąt równoramienny");
         }
     }
+
+               public static boolean czyIstniejeTrojkat(float a, float b, float c){
+                  if (a + b <= c) return false;
+                  if (a + c <= b) return false;
+                  if (b + c <= a) return false;
+                  return true;
+               }
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
@@ -41,7 +49,10 @@ class Trojkaty {
             System.out.println("Długości boków trójkąta muszą być nieujemne!");
             System.exit(2);
         }
-
+                   if (!czyIstniejeTrojkat(a, b, c)) {
+                    System.out.println("Z podanych długości boków nie da się zbudować trójkąta");
+                    System.exit(3);
+                   }
         jakiTrojkat(a, b, c);
     }
 }
