a51eb3da9139f294a6ec7c7adb020483643fcdc2
Marcel Seweryn <masewera@artemis.wszib.edu.pl>
2022-06-19 03:06:40 +0200

    Optymalizacja: uwspólnienie kodu

--- trojkaty.java
+++ trojkaty.java
@@ -9,23 +9,34 @@ class Trojkaty {
      * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
-    public static void jakiTrojkat(float a, float b, float c){
-        if (a == b && b == c && a == c) {
-            System.out.println("Trójkąt równoboczny"); 
-        }
-        if (a == b && b == c && a == c) {
-            System.out.println("Trójkąt równoboczny");
-        }
-        if (a == b || b == c || a == c) {
-            System.out.println("Trójkąt równoramienny");
-        }
-    }
+
+               public static int ileJednakowych(float a, float b, float c){
+                   int jednakowe = 0; // tutaj zliczamy ile jest jednakowych bokow
+                   if (a == b) {
+                      jednakowe++;
+                   }
+                   if (b == c) {
+                      jednakowe++;
+                   }
+                   return jednakowe;
+               }
+
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
         System.out.println("Program do rozpoznawania rodzaju trójkąra");
         System.out.println("Uruchom z trzema argumentami liczbowymi - długość boków trójkąta");
     }
+
+              public static void jakiTrojkat(float a, float b, float c){
+                   int jednakoweDlugosci = ileJednakowych(a, b, c); // tutaj zliczamy ile jest jednakowych bokow
+                   if (jednakoweDlugosci > 1) {
+                       System.out.println("Trójkąt równoboczny"); 
+                   } else if (jednakoweDlugosci > 0) {
+                       System.out.println("Trójkąt równoramienny");
+                   }
+		}
+
     /** Glowna funkcja */
     public static void main(String... args) {
         if (args.length != 3) {
